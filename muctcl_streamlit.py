a8a52c0d0986b81c106edfc529c9856874090f74
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-10-30 22:40:25 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -166,6 +166,7 @@ if submit_button:
                 st.markdown(f"<h1 style='{style_heading}'>MOLECULE</h1>", unsafe_allow_html=True)
                 st.markdown(f"<h1 style='{style_heading}'>ISOLATED:</h1>", unsafe_allow_html=True)
                 st.markdown(f"<h1 style='{style_heading}'></h1>", unsafe_allow_html=True)
+                st.markdown(f"<h1 style='{style_heading}'></h1>", unsafe_allow_html=True)
                 st.markdown(f"<h1 style='{style_heading}'>MIXTURE:</h1>", unsafe_allow_html=True)
             with col2: 
                 st.image("tc.png")
