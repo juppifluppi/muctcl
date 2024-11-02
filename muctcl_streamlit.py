a50ec1be2b2772a302ac308b03d8ca1a448143ea
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-11-02 15:03:07 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -188,7 +188,7 @@ if submit_button:
                 st.image(im)
                 st.markdown(f"<h1 style='{style_heading}'>BILE PREDICTION</h1>", unsafe_allow_html=True)
                 liquidfill_option = {
-                "series": [{"type": "liquidFill", "data": [int(tcl3)]}]
+                "series": [{"type": "liquidFill", "data": [int(tcl3*100)]}]
                 }
                 st_echarts(liquidfill_option,key="23456")
                 #st.image("tc.png",width=80)
