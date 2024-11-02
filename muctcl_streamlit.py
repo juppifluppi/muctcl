ca3f4600673e9edd27343ee92273cd3b469eb762
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-11-02 11:21:37 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -174,7 +174,7 @@ if submit_button:
                 "series": [{"type": "liquidFill", "data": [int(round(tcl3*100,2))]}]
                 }
                 st_echarts(liquidfill_option,key="23456")
-                st.image("tc.png")
+                st.image("tc.png",width=200)
                
             with col2: 
                 st.markdown(f"<h1 style='{style_heading}'>MIXTURE PREDICTION</h1>", unsafe_allow_html=True)
@@ -197,7 +197,6 @@ if submit_button:
                                 "label": {"show": True, "fontSize": "20", "fontWeight": "bold"}
                             },
                             "labelLine": {"show": False},
-                            "color": {"#fff","#fff","#fff","#fff"},
                             "data": [
                                 {"value": int(df3.iloc[0, 0]*100), "name": "bile+mucin interacting"},
                                 {"value": int(df4.iloc[0, 0]*100), "name": "mucin interacting"},
@@ -215,7 +214,7 @@ if submit_button:
                 "series": [{"type": "liquidFill", "data": [int(df2.iloc[0, 0]*1)]}]
                 }
                 st_echarts(liquidfill_option2,key="3456")                                                         
-                st.image("muc2.png")
+                st.image("muc2.png",width=200)
 
             for es in ["descriptors.csv","results.csv","results2.csv","results3.csv","results4.csv","results5.csv"]:
                 try:
