fb6a5ed5461c232cc7f3092657c44167a463ec92
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-11-02 14:58:26 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -147,9 +147,6 @@ if submit_button:
                 tcl1 = ( ( logd - 1.510648) / 1.708574 ) * 1.706694
                 tcl2 = ( ( mr - 90.62889 ) / 35.36033 ) * 2.4925333    
                 tcl3 = 1 / ( 1 + ( 2.718281828459045 ** ( -1 * ( 0.9872289 + tcl1 + tcl2 ) ) ) )   
-                st.write(tcl1)
-                st.write(tcl2)
-                st.write(tcl3)
                 #st.write("logD: " + str(round(logd,2)))
                 #st.write("CrippenMR: " + str(round(mr,2)))
                 #st.write("TC/L interaction probability: " + str(int(round(tcl3*100,2))) + " %")
@@ -191,7 +188,7 @@ if submit_button:
                 st.image(im)
                 st.markdown(f"<h1 style='{style_heading}'>BILE PREDICTION</h1>", unsafe_allow_html=True)
                 liquidfill_option = {
-                "series": [{"type": "liquidFill", "data": [int(round(tcl3*100,2))]}]
+                "series": [{"type": "liquidFill", "data": [int(round(tcl3,2))]}]
                 }
                 st.write(tcl3)
                 st_echarts(liquidfill_option,key="23456")
@@ -271,7 +268,7 @@ if submit_button:
                tcl1 = ( ( logd - 1.510648) / 1.708574 ) * 1.706694
                tcl2 = ( ( mr - 90.62889 ) / 35.36033 ) * 2.4925333    
                tcl3 = 1 / ( 1 + ( 2.718281828459045 ** ( -1 * ( 0.9872289 + tcl1 + tcl2 ) ) ) )     
-               omoo.append(int(round(tcl3*100,2)))
+               omoo.append(int(round(tcl3,2)))
     
                if cx == 0:
 
