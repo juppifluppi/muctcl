0c18e8209e8db0b5196641c4bc9db1ded089a767
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-11-02 15:47:13 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -31,6 +31,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from dimorphite_dl import DimorphiteDL
 
+def show_mol(d2d,mol,legend='',highlightAtoms=[]):
+    d2d.DrawMolecule(mol,legend=legend, highlightAtoms=highlightAtoms)
+    d2d.FinishDrawing()
+    bio = BytesIO(d2d.GetDrawingText())
+    return Image.open(bio)
+def show_images(imgs,buffer=5):
+    height = 0
+    width = 0
+    for img in imgs:
+        height = max(height,img.height)
+        width += img.width
+    width += buffer*(len(imgs)-1)
+    res = Image.new("RGBA",(width,height))
+    x = 0
+    for img in imgs:
+        res.paste(img,(x,0))
+        x += img.width + buffer
+    return res
+
 dimorphite_dl = DimorphiteDL(
     min_ph = 6.4,
     max_ph = 6.6,
