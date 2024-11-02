6a7623d39e37b847680c11eefa43886f7494a87c
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-11-02 14:59:29 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -188,9 +188,8 @@ if submit_button:
                 st.image(im)
                 st.markdown(f"<h1 style='{style_heading}'>BILE PREDICTION</h1>", unsafe_allow_html=True)
                 liquidfill_option = {
-                "series": [{"type": "liquidFill", "data": [int(round(tcl3,2))]}]
+                "series": [{"type": "liquidFill", "data": [int(round(tcl3*100,2))]}]
                 }
-                st.write(tcl3)
                 st_echarts(liquidfill_option,key="23456")
                 #st.image("tc.png",width=80)
                 st.markdown("<img src='https://raw.githubusercontent.com/juppifluppi/muctcl/refs/heads/main/tc.png' width='150' style='display: block; margin: 0 auto;'>" , unsafe_allow_html=True)
@@ -268,7 +267,7 @@ if submit_button:
                tcl1 = ( ( logd - 1.510648) / 1.708574 ) * 1.706694
                tcl2 = ( ( mr - 90.62889 ) / 35.36033 ) * 2.4925333    
                tcl3 = 1 / ( 1 + ( 2.718281828459045 ** ( -1 * ( 0.9872289 + tcl1 + tcl2 ) ) ) )     
-               omoo.append(int(round(tcl3,2)))
+               omoo.append(int(round(tcl3*100,2)))
     
                if cx == 0:
 
