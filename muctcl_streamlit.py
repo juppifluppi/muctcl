d3950fcb5786c90b277441b6668227e3ed58f6df
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-11-02 11:24:37 +0100

    Update muctcl_streamlit.py

--- muctcl_streamlit.py
+++ muctcl_streamlit.py
@@ -169,13 +169,16 @@ if submit_button:
                 im = Draw.MolToImage(Chem.MolFromSmiles(SMI),fitImage=True)
                 st.image(im)
 
-                st.markdown(f"<h1 style='{style_heading}'>bile prediction</h1>", unsafe_allow_html=True)
-                liquidfill_option = {
-                "series": [{"type": "liquidFill", "data": [int(round(tcl3*100,2))]}]
-                }
-                st_echarts(liquidfill_option,key="23456")
-                st.image("tc.png",width=200)
-               
+                col1x, col2x = st.columns(2)
+                with col1x:
+                    st.markdown(f"<h1 style='{style_heading}'>bile prediction</h1>", unsafe_allow_html=True)
+                    st.image("tc.png",width=200)
+                with col2x:
+                    liquidfill_option = {
+                    "series": [{"type": "liquidFill", "data": [int(round(tcl3*100,2))]}]
+                    }
+                    st_echarts(liquidfill_option,key="23456")
+                 
             with col2: 
                 st.markdown(f"<h1 style='{style_heading}'>MIXTURE PREDICTION</h1>", unsafe_allow_html=True)
                 options = {
